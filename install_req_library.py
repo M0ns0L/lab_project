07d86aacc04452c2ff1810af161ac6e7c701cd7d
M0ns0L <milansagolsem00@gmail.com>
2025-05-01 17:57:37 +0530

    install_req_library.py

--- install_req_library.py
+++ install_req_library.py
@@ -1,1 +1,25 @@
 
+#pip install flask flask-sqlalchemy flask-wtf flask-bcrypt flask-login flask-admin
+#pip install pandas
+
+commands = [
+    'pip install pandas',
+    'pip install flask flask-sqlalchemy flask-wtf flask-bcrypt flask-login flask-admin'
+]
+import subprocess
+def run_commands(commands):
+    for command in commands:
+        
+        # Run each command
+        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        
+        # Get the output and error (if any)
+        output, error = process.communicate()
+        
+        # Decode and print the output
+        print("Output:\n", output.decode())
+        
+        # Print errors, if any
+        if error:
+            print("Error:\n", error.decode())
+run_commands(commands)
