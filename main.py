5eb8bd5c98aa132c7fa4cd9c1d61c9d126679ed5
M0ns0L <milansagolsem00@gmail.com>
2025-05-01 17:15:36 +0530

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import mqsql.connector 
 import pandas as pd
+
+connection = mysql.connector.connect(
+    host="localhost",
+    user="your_username",  # MySQL username
+    password="your_password",  # MySQL password
+    database="school_db"
+)
