3daae7662a5787c6426d0cee6f0d701f896a0764
M0ns0L <milansagolsem00@gmail.com>
2025-05-06 16:47:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import mqsql.connector
 import pandas as pd
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
-@app.route("/")
+
 connection = mysql.connector.connect(
     host="localhost",
     user="your_username",  # MySQL username
@@ -10,6 +10,17 @@ connection = mysql.connector.connect(
     database="school_db"
 )
 
+@app.route("/home/")
 def home_page():
     return render_template("index.html")
-app.run(debug=True)
+
+@app.route("/start_practical/")
+def start_prac():
+    return render_templates("start_prac.html")
+
+@app.route("/student_record/")
+def student_record():
+    return render_templates("student_record.html")
+
+
+app.run(debug=True, port = 80)
